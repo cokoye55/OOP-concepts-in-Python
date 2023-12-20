927a604da9f411db5d9ea59d534798cd3212cf03
CJ Okoye <153961310+cokoye55@users.noreply.github.com>
2023-12-20 17:16:35 -0600

    Update classes.py

--- expense_tracker/classes.py
+++ expense_tracker/classes.py
@@ -2,7 +2,7 @@
 import uuid 
 from datetime import datetime, timezone
 
-# creating the Expense class
+# Creating the Expense class which represents an individual financial expense.
 class Expense:
     def __init__(self, title, amount):
         self.id = str(uuid.uuid4())
@@ -11,7 +11,7 @@ class Expense:
         self.created_at = datetime.utcnow()
         self.updated_at = self.created_at
         
-        
+# Updates the title and/or amount of the expense.       
     def update(self, title = None, amount = None):
         if title is not None:
             self.title = title
@@ -19,8 +19,7 @@ class Expense:
             self.amount = amount
         self.updated_at = datetime.utcnow()
             
-
-# Creating a dictionary representation of the Expense Class
+# Creating a dictionary representation of the Expense Class.
     def to_dict(self):
         return {
         "id": self.id,
@@ -30,37 +29,31 @@ class Expense:
         "updated_at": self.updated_at.isoformat()
         }
         
-        
-
-# Creating Expense Database class        
+# Creating an Expense Database class for managing a collection of Expense objects.       
 class ExpenseDatabase:
     def __init__(self):
         self.expenses = []
 
-# Adding an Expense 
+# Adding a new expense to the ExpenseDatabase. 
     def add_expense(self, expense):
         self.expenses.append(expense)
     
-
-# Removing an expense from ExpenseDatabase    
+# Removing an expense from ExpenseDatabase by a unique ID     
     def remove_expense(self, expense_id):
         self.expenses = [expense for expense in self.expenses if expense.id != expense_id]
     
-# Getting an expense by ID.    
-
+# Retriving an expense by its unique ID.    
     def get_expense_by_id(self, expense_id):
         for expense in self.expenses:
             if expense.id == expense_id:
                 return expense
-            return None
+        return None
     
-# Get expenses by title (returning a list).
-
+# Retriving Expenses by title (returning a list).
     def get_expenses_by_title(self, title):
         return [expense for expense in self.expenses if expense.title == title]
 
-# Createing a to_dict method that returns a list of dictionaries representing each expense in 
-# #the database.
-
+# Creating a to_dict method that returns a list of dictionaries representing each expense in 
+# the database
     def to_dict(self):
         return [expense.to_dict() for expense in self.expenses]
